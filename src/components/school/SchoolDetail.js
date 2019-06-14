c62c6b801e71a06e6f3c5680b16ebcae0a1758a4
Burak Kaya Koc <burakkayakoc@gmail.com>
2019-06-14 12:04:20 -0500

    teacher card created

--- src/components/school/SchoolDetail.js
+++ src/components/school/SchoolDetail.js
@@ -1,69 +1,40 @@
-import React, { Component } from "react"
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+//import { Link } from "react-router-dom";
 import TeacherCard from "../teacher/TeacherCard";
 
-
 export default class SchoolDetail extends Component {
-    render() {
+  render() {
+    const school = this.props.schools.find(
+      (a) => a.id === parseInt(this.props.match.params.schoolId)
+    ) || { id: 404, name: "404", grade: "School not found" };
 
-        const school = this.props.schools.find(a =>
-            a.id === parseInt(this.props.match.params.schoolId))
-             || {id:404, name:"404", grade: "School not found"}
+    return (
+      <section className="">
+        <div key={school.id} className="card">
+          <div className="card-body">
+            <h2 className="card-title">{school.name}</h2>
+            <h6 className="card-title">Address: {school.location}</h6>
+            <h6 className="card-title">Phone Number: {school.phone}</h6>
+            <h6 className="card-title">
+              Number of Students: {school.numberStudents}
+            </h6>
 
-        return (
             <section className="">
-                <div key={school.id} className="card">
-                    <div className="card-body">
-                        <h2 className="card-title">
-                            {school.name}
-                        </h2>
-                        <h6 className="card-title">
-                            Address: {school.location}
-                        </h6>
-                        <h6 className="card-title">
-                           Phone Number: {school.phone}
-                        </h6>
-                        <h6 className="card-title">
-                           Number of Students: {school.numberStudents}
-                        </h6>
-                        
-
-                        <h6 className="">Students</h6>
-
-                        
-                        
-                        
-        <section className="">
-          {this.props.schools.map((school) => (
-            <div key={`school-${school.id}`} school={school} >
-              <div className="">
-                <Link className="school-link" to={`/schools/${school.id}`}>
-                  {school.name}
-                </Link>
-                </div>
-          
-              <h6 className="">Students</h6>
+              <h6 className="">Teachers:</h6>
               <div className="">
                 {this.props.teachers
-                
+
                   .filter((std) => std.schoolId === school.id)
                   .map((std) => (
-                    <TeacherCard key={std.id} teacher={std} {...this.props}  />
+                    <TeacherCard key={std.id} teacher={std} {...this.props} />
                   ))}
               </div>
-            </div>
-          ))}
-        </section>
-            
-
-                        
-                    
-                                <div className="">
-                                
-                        </div>
-                    </div>
-                </div>
             </section>
-        )
-    }
+
+            <div className="" />
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
