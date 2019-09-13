4eee41c35043ca7240e4770347cbac4720a22ade
Burak Koc <burakkayakoc@gmail.com>
2019-09-13 18:09:50 -0500

    azure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const dbPath = "./API/db.json";
+const dbPath = "./API/database.json";
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(dbPath);
