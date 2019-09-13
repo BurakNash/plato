8fc64f6d61c8294a54eee860a3340f4c632c79ef
Burak Koc <burakkayakoc@gmail.com>
2019-09-13 10:51:29 -0500

    updates

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const path = require("path");
+const dbPath = "./API/db.json";
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("database.json");
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router(dbPath);
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 5002;
 
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1"
+  })
+);
+
+server.use((req, res, next) => {
+  // use originalUrl since other middleware is likely reassigning req.url
+  const isApiRoute = req.originalUrl.includes("/api/");
+
+  if (isApiRoute) return next();
+
+  return res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
 server.use(router);
-server.listen(process.env.PORT, () => {
-  console.log("JSON Server is running");
+
+server.listen(port, () => {
+  console.log(`app running on port ${port}`);
 });
