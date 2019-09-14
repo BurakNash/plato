de300be427270a74eed6adf965cd445395cdd3e6
Burak Koc <burakkayakoc@gmail.com>
2019-09-13 21:20:41 -0500

    all files

---
+++ build/precache-manifest.bc824a24dcc8589d3854851ff852dcca.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3452145c7289b2c2e32e537ceec9f862",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5d3ee146bcd947f73c8",
+    "url": "/static/css/2.0601dd10.chunk.css"
+  },
+  {
+    "revision": "5ebdb75056f2d4711ab6",
+    "url": "/static/css/main.d0cd005e.chunk.css"
+  },
+  {
+    "revision": "e5d3ee146bcd947f73c8",
+    "url": "/static/js/2.b4b4a572.chunk.js"
+  },
+  {
+    "revision": "5ebdb75056f2d4711ab6",
+    "url": "/static/js/main.ed9c2fed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75d208e9f689bb93e01991caf4da3c8a",
+    "url": "/static/media/LOGO.75d208e9.jpg"
+  },
+  {
+    "revision": "210caef1c29e343855f71906d3de8658",
+    "url": "/static/media/background.210caef1.jpg"
+  },
+  {
+    "revision": "190e72572ee84190f4777765fa49fc47",
+    "url": "/static/media/octicons.190e7257.svg"
+  },
+  {
+    "revision": "d038ccbc4a99be24f33a54b482b2422e",
+    "url": "/static/media/octicons.d038ccbc.eot"
+  },
+  {
+    "revision": "de59a97248b44599e6747a27a943f738",
+    "url": "/static/media/octicons.de59a972.woff2"
+  },
+  {
+    "revision": "e0d4a324833e13be7d4fa762146d0a71",
+    "url": "/static/media/octicons.e0d4a324.ttf"
+  },
+  {
+    "revision": "ee5b1bee959a95bd43b223ec901d098a",
+    "url": "/static/media/octicons.ee5b1bee.woff"
+  },
+  {
+    "revision": "59a7254b55fa63e2ef8286051ec4b937",
+    "url": "/static/media/schools.59a7254b.gif"
+  },
+  {
+    "revision": "5178bbd62e356659fc8acd19eb0f65b8",
+    "url": "/static/media/students.5178bbd6.gif"
+  },
+  {
+    "revision": "a950f89012fc5ae54d232b0336fa2cda",
+    "url": "/static/media/teachers.a950f890.gif"
+  }
+]);
