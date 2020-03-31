9f1c80506102ff1f38a312d24cd936073bf9d21e
Burak <burakkayakoc@gmail.com>
2020-03-31 09:50:35 -0600

    baseurl edit

--- src/modules/Settings.js
+++ src/modules/Settings.js
@@ -1,4 +1,6 @@
 export default {
   remoteURL:
-    process.env.NODE_ENV === "production"?"/api":"http://localhost:5002/api"
+  process.env.NODE_ENV === 'production'
+  ? "/api/"
+  : "http://localhost:5002/api/"
 };
